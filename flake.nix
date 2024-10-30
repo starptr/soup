4e001eb4bbf7587169feed8da8069716eca57f92
Yuto Nishida <yuto@berkeley.edu>
2024-10-30 14:24:01 -0700

    Add overlays and aarch64-darwin

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
         "x86_64-linux"
         "i686-linux"
         "x86_64-darwin"
+        "aarch64-darwin"
         "aarch64-linux"
         "armv6l-linux"
         "armv7l-linux"
@@ -18,5 +19,6 @@
         pkgs = import nixpkgs { inherit system; };
       });
       packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+      overlays = import ./overlays;
     };
 }
--- overlays/default.nix
+++ overlays/default.nix
@@ -3,4 +3,7 @@
   #
   # my-overlay = import ./my-overlay;
   expand-love-overlay = import ./expand-love.nix;
+
+  # This overlay naively adds all of the pkgs.
+  #default = import ../overlay.nix;
 }
