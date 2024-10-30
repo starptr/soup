a4d46bfe87b960fb9135889238bbc9e5abcc6fcb
Yuto Nishida <yuto@berkeley.edu>
2024-10-30 14:25:26 -0700

    Rename overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,7 @@
   # Add your overlays here
   #
   # my-overlay = import ./my-overlay;
-  expand-love-overlay = import ./expand-love.nix;
+  expand-love = import ./expand-love.nix;
 
   # This overlay naively adds all of the pkgs.
   #default = import ../overlay.nix;
